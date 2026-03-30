b912fa6df9982b746737929965d7078adb6191ae
Wanessa Silva <wanessa.silva33@aluno.ce.gov.br>
2026-03-30 19:56:57 +0000

    feito

---
+++ 1-bimestre/programacao-web/ex11-aprendendo-dom/src/js/script.js
@@ -0,0 +1,7 @@
+const titulo = document.getElementById("titulo");
+
+const botao = document.getElementById("botao")
+
+botao.addEventListener("click", () => {
+    titulo.textContent = "Texto Alterado com JavaScript";
+});
